eb94f89fdccf34a5aa039ecd89b271f5a9431b03
Ryvin0 <cwalicek7@gmail.com>
2020-03-02 19:47:39 -0500

    Final Functionality Test Cases for R1 and R2

--- TestTrafficClass.java
+++ TestTrafficClass.java
@@ -12,6 +12,11 @@ public class TestTrafficClass
     //Global variables
     public List<Traffic> loc;
     
+    /*
+        Start of R1 functionality Test Cases
+    */
+    
+    
     //Test the connect() method of Traffic class
     @Test
     public void testConnect()
@@ -66,21 +71,6 @@ public class TestTrafficClass
         assertTrue(test.equals(loc));
     }
     
-    
-    //Test to make sure trafficLocations contains searched location 
-    @Test
-    public void checkLocations() 
-    {
-
-
-    }
-    
-    //Test to make validate size of incidents 
-    @Test 
-    public void checkIncidents() 
-    { 
-    	
-    }
     
     // This is the start of the R2 Functionality test suite
 
